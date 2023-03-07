c0a02e0035fba7d947384e1100c062286af86e60
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2023-03-07 16:33:39 +0100

    reformatting using google style

--- cpp-implementation/src/vocabs.cpp
+++ cpp-implementation/src/vocabs.cpp
@@ -9,67 +9,67 @@
 #include <Eigen/Dense>
 
 void get_word_to_index(
-        std::unordered_map<std::string,int> &word_to_index,
-        const std::string filename) {
+    std::unordered_map<std::string,int> &word_to_index,
+    const std::string filename) {
 
-    std::ifstream file(filename);
-    int i = 0;
-    for(std::string line; std::getline(file, line); ++i) {
-        if(word_to_index.count(line) != 0) {
-            std::cerr << "Duplicate entry in vocabulary: '" 
-                      << line << "' on line " << i << std::endl; 
-            std::abort();
-        }
-        word_to_index.insert({line, i});
+  std::ifstream file(filename);
+  int i = 0;
+  for(std::string line; std::getline(file, line); ++i) {
+    if(word_to_index.count(line) != 0) {
+      std::cerr << "Duplicate entry in vocabulary: '"
+                << line << "' on line " << i << std::endl;
+      std::abort();
     }
+    word_to_index.insert({line, i});
+  }
 
-    file.close();
+  file.close();
 }
 
 Vocab::Vocab(const std::string& filename) {
-    std::ifstream file(filename);
-    int i = 0;
-    for(std::string line; std::getline(file, line); ++i) {
-        if(word_to_index.count(line) != 0) {
-            std::cerr << "Duplicate entry in vocabulary: '" 
-                      << line << "' on line " << i << std::endl; 
-            std::abort();
-        }
-
-        word_to_index.insert({line, i});
-        index_to_word.push_back(line);
+  std::ifstream file(filename);
+  int i = 0;
+  for(std::string line; std::getline(file, line); ++i) {
+    if(word_to_index.count(line) != 0) {
+      std::cerr << "Duplicate entry in vocabulary: '"
+                << line << "' on line " << i << std::endl;
+      std::abort();
     }
+
+    word_to_index.insert({line, i});
+    index_to_word.push_back(line);
+  }
 }
 
 Embeddings::Embeddings(const std::string& filename) {
-    std::ifstream embedding_fh(filename);
+  std::ifstream embedding_fh(filename);
 
-    std::string firstline;
-    std::getline(embedding_fh, firstline);
-    std::stringstream ss_firstline(firstline);
-    ss_firstline >> word_count;
-    ss_firstline >> embedding_dim;
+  std::string firstline;
+  std::getline(embedding_fh, firstline);
+  std::stringstream ss_firstline(firstline);
+  ss_firstline >> word_count;
+  ss_firstline >> embedding_dim;
 
-    emb.resize(word_count, embedding_dim);
-    index_to_word.resize(word_count);
+  emb.resize(word_count, embedding_dim);
+  index_to_word.resize(word_count);
 
-    int i = 0;
-    for(std::string line; std::getline(embedding_fh, line); ++i) {
-        std::stringstream ss(line);
-        std::string word;
-        ss >> word;
-        
-        if(word_to_index.count(line) != 0) {
-            std::cerr << "Duplicate entry in vocabulary: '"
-                      << line << "' on line " << i << std::endl; 
-            std::abort();
-        }
+  int i = 0;
+  for(std::string line; std::getline(embedding_fh, line); ++i) {
+    std::stringstream ss(line);
+    std::string word;
+    ss >> word;
 
-        word_to_index.insert({word, i});
-        index_to_word[i] = word;
+    if(word_to_index.count(line) != 0) {
+      std::cerr << "Duplicate entry in vocabulary: '"
+                << line << "' on line " << i << std::endl;
+      std::abort();
+    }
 
-        for(int j = 0; j < embedding_dim; ++j) {
-            ss >> emb(i, j);
-        }
-    }    
+    word_to_index.insert({word, i});
+    index_to_word[i] = word;
+
+    for(int j = 0; j < embedding_dim; ++j) {
+      ss >> emb(i, j);
+    }
+  }
 }
--- cpp-implementation/src/vocabs.h
+++ cpp-implementation/src/vocabs.h
@@ -11,31 +11,33 @@ void get_word_to_index(
 
 
 class Vocab {
-protected:
-    Vocab() {}
+ protected:
+  Vocab() {}
 
-public:
-    std::unordered_map<std::string, int> word_to_index;
-    std::vector<std::string> index_to_word;
+ public:
+  std::unordered_map<std::string, int> word_to_index;
+  std::vector<std::string> index_to_word;
 
-    int size() const { return word_to_index.size(); }
-    bool contains(const std::string& word) const { return word_to_index.count(word) == 1; }
+  int size() const { return word_to_index.size(); }
+  bool contains(const std::string& word) const { return word_to_index.count(word) == 1; }
 
-    int operator[](const std::string& word) const { return word_to_index.at(word); }
-    const std::string& operator[](int index) const { return index_to_word[index]; }
+  int operator[](const std::string& word) const { return word_to_index.at(word); }
+  const std::string& operator[](int index) const { return index_to_word[index]; }
 
-    Vocab(const std::string& filename);
-    virtual ~Vocab() {};
+
+
+  Vocab(const std::string& filename);
+  virtual ~Vocab() {};
 };
 
 
 class Embeddings : public Vocab {
-private:
-    int word_count;
+ private:
+  int word_count;
 
-public:
-    int embedding_dim;
-    Eigen::MatrixXf emb;
+ public:
+  int embedding_dim;
+  Eigen::MatrixXf emb;
 
-    Embeddings(const std::string &filename);
+  Embeddings(const std::string &filename);
 };
