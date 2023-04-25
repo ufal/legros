f64a1d0cca0261c91d04cb97f402c0be0cf55f66
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2023-04-25 17:16:20 +0200

    trials with intel mkl support. does not work yet i think

--- cpp-implementation/src/test-eigen.cpp
+++ cpp-implementation/src/test-eigen.cpp
@@ -1,7 +1,13 @@
 #include <Eigen/Dense>
 #include <iostream>
 
+#define EIGEN_USE_MKL_ALL
+
 int main(int argc, char* argv[]) {
+
+  std::cout << "Eigen version: " << EIGEN_WORLD_VERSION << "."
+            << EIGEN_MAJOR_VERSION << "." << EIGEN_MINOR_VERSION << std::endl;
+  std::cout << "Using backend: " << Eigen::SimdInstructionSetsInUse() << std::endl;
 
   auto m1 = Eigen::MatrixXf::Random(25000, 197000);
   auto m2 = Eigen::MatrixXf::Random(197000, 200);
